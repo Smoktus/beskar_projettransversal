d7765de38c423091ca89b1d635f1d3899968b6bb
Stef <stephane.paput@insa-lyon.fr>
2021-04-09 15:34:51 +0200

    test stef

--- bin/db/connection.dart
+++ bin/db/connection.dart
@@ -6,16 +6,16 @@ class DB {
 
   DB() {
     Map<String, String> envVars = Platform.environment;
-    var host = envVars["DB_HOST"] == null ? "ec2-52-213-167-210.eu-west-1.compute.amazonaws.com" : envVars["DB_HOST"];
+    var host = envVars["DB_HOST"] == null ? "localhost" : envVars["DB_HOST"];
     var port =
         envVars["DB_PORT"] == null ? 5432 : int.parse(envVars["DB_PORT"]);
     var database = envVars["DB_DATABASE"] == null
-        ? "d2pqn9a2sjvng6"
+        ? "dbbeskarfinal"
         : envVars["DB_DATABASE"];
     var username =
-        envVars["DB_USERNAME"] == null ? "wwqyeegwbauyji" : envVars["DB_USERNAME"];
+        envVars["DB_USERNAME"] == null ? "postgres" : envVars["DB_USERNAME"];
     var password =
-        envVars["DB_PASSWORD"] == null ? "52e47109d13fcb8c6038790bd48f07ce0578370089e44da721a50e66348f9314" : envVars["DB_PASSWORD"];
+        envVars["DB_PASSWORD"] == null ? "beskar" : envVars["DB_PASSWORD"];
     this.conn = PostgreSQLConnection(host, port, database,
         username: username, password: password);
   }
